a4ed0e19f9ebaadda74e45c777933ea0df076c3e
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-10-01 21:24:57 +0530

    Implement Entity

---
+++ src/main/java/com/praveen/blogapp/comments/CommentsRepository.java
@@ -0,0 +1,9 @@
+package com.praveen.blogapp.comments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<CommentEntity,Long> {
+
+}
---
+++ src/main/java/com/praveen/blogapp/users/UserEntity.java
@@ -0,0 +1,41 @@
+package com.praveen.blogapp.users;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+@Entity(name = "users")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    @NonNull
+    private String username;
+
+    @Column(nullable = false)
+    @NonNull
+    private String password;
+
+    @Column(nullable = false)
+    @NonNull
+    private String email;
+
+    @Column(nullable = true)
+    @Nullable
+    private String bio;
+
+    @Column(nullable = true)
+    @Nullable
+    private String image;
+
+}
